df2d254b7cd0ff663459042ed5fb63c6f4bddc36
joshuapclarke <joshuapclarke@icloud.com>
2026-04-03 17:10:15 -0600

    removed a couple lines

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './CartContext';
 import { Store } from './Store';
--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BookList } from './BookList';
 import { Cart } from './Cart';
 
